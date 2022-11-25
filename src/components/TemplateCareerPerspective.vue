34faba936df332bae3580238328fd5da084f77a8
MirkoFarina <MirkoFarina@outlook.com>
2022-11-25 13:31:34 +0100

    [add] responsive

--- src/components/TemplateCareerPerspective.vue
+++ src/components/TemplateCareerPerspective.vue
@@ -40,6 +40,7 @@ export default {
             top: 50px;
             left: -50px;
             background-image: url('../assets/img/underlay-shape-lilla.svg');
+            background-size: 60% 70%;
             background-repeat: no-repeat;
             width: 100%;
             height: 100%;
