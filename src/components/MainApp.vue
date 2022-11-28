7b45c4fc6e8d55404425a14441415f9d195226d7
MirkoFarina <MirkoFarina@outlook.com>
2022-11-28 12:04:15 +0100

    [ADD] WAVE

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,92 +1,129 @@
 <script>
-import {dataCardDream, dataAccordion, dataStatics, dataCardCourses,dataEvents, dataCardsCoach} from '../data/allDataSection';
+import {
+  dataCardDream,
+  dataAccordion,
+  dataStatics,
+  dataCardCourses,
+  dataEvents,
+  dataCardsCoach,
+} from "../data/allDataSection";
 //JUMBO
-import JumbotronApp from './JumbotronApp.vue';
-// BUILD DREAM 
-import TemplateBuildDream from './TemplateBuildDream.vue';
+import JumbotronApp from "./JumbotronApp.vue";
+// BUILD DREAM
+import TemplateBuildDream from "./TemplateBuildDream.vue";
 // CARRER PERSPECTIVE
-import TemplateCareerPerspective from './TemplateCareerPerspective.vue';
+import TemplateCareerPerspective from "./TemplateCareerPerspective.vue";
 // STATICS
-import StaticsApp from './StaticsApp.vue';
+import StaticsApp from "./StaticsApp.vue";
 // COURSES
-import TemplateCourses from './TemplateCourses.vue';
+import TemplateCourses from "./TemplateCourses.vue";
 // QUESTION
-import QuestionLeft from './QuestionLeft.vue';
-import QuestionRight from './QuestionRight.vue';
+import QuestionLeft from "./QuestionLeft.vue";
+import QuestionRight from "./QuestionRight.vue";
 //EVENTS
-import TemplateEvent from './TemplateEvent.vue';
+import TemplateEvent from "./TemplateEvent.vue";
 // COACH
-import TemplateCoach from './TemplateCoach.vue';
+import TemplateCoach from "./TemplateCoach.vue";
 
-  export default {
-    name: 'MainApp',
-    components: {
-      JumbotronApp,
-      TemplateBuildDream,
-      TemplateCareerPerspective,
-      StaticsApp,
-      TemplateCourses,
-      QuestionLeft,
-      QuestionRight,
-      TemplateEvent,
-      TemplateCoach
-    },
-    data(){
-        return {
-            dataCardDream,
-            dataAccordion,
-            dataStatics,
-            dataCardCourses,
-            dataEvents,
-            dataCardsCoach
-        }
-    }
-  }
+export default {
+  name: "MainApp",
+  components: {
+    JumbotronApp,
+    TemplateBuildDream,
+    TemplateCareerPerspective,
+    StaticsApp,
+    TemplateCourses,
+    QuestionLeft,
+    QuestionRight,
+    TemplateEvent,
+    TemplateCoach,
+  },
+  data() {
+    return {
+      dataCardDream,
+      dataAccordion,
+      dataStatics,
+      dataCardCourses,
+      dataEvents,
+      dataCardsCoach,
+    };
+  },
+};
 </script>
 
 <template>
-    <main>
-        <jumbotron-app class="mb-5" />
-        
-        <section id="build-dream">
-            <TemplateBuildDream :data="dataCardDream" />
-        </section>
+  <main>
+    <jumbotron-app class="mb-5" />
 
-        <section id="career-perspective">
-            <TemplateCareerPerspective :dataAccordion="dataAccordion"/>
-        </section>
+    <section id="build-dream">
+      <TemplateBuildDream :data="dataCardDream" />
+    </section>
 
-        <section id="statistics">
-            <StaticsApp :statics="dataStatics" />
-        </section>
+    <section id="career-perspective">
+      <TemplateCareerPerspective :dataAccordion="dataAccordion" />
+    </section>
 
-        <section id="courses">
-            <TemplateCourses :dataCourses="dataCardCourses"/>
-        </section>
+    <section id="statistics">
+      <StaticsApp :statics="dataStatics" />
+    </section>
 
-        <section id="questions">
-            <QuestionLeft/>
-            <QuestionRight />
-        </section>
+    <section id="courses">
+      <TemplateCourses :dataCourses="dataCardCourses" />
+    </section>
 
-        <section id="events">
-            <TemplateEvent :dataCard="dataEvents" />
-        </section>
+    <!-- WAVE -->
+    <div class="position-relative">
+      <div class="bg-image rounded-7 shadow-5">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+          style="
+            opacity: 1;
+            width: 260%;
+            height: 130px;
+            fill: #f6f6f6;
+            transform: rotateY(0deg);
+          "
+        >
+          <path
+            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+          ></path>
+        </svg>
+      </div>
+    </div>
+    <!-- /WAVE -->
+    
+    <section id="questions">
+      <QuestionLeft />
+      <QuestionRight />
+    </section>
 
-        <section id="coach">
-            <TemplateCoach :dataCards="dataCardsCoach"/>
-        </section>
-    </main>
+    <section id="events">
+      <TemplateEvent :dataCard="dataEvents" />
+    </section>
+
+    <section id="coach">
+      <TemplateCoach :dataCards="dataCardsCoach" />
+    </section>
+  </main>
 </template>
 
 
 <style lang="scss" scoped>
-@use '../scss/partials/variables' as *;
+@use "../scss/partials/variables" as *;
 section {
   margin-bottom: 100px;
 }
 
+#courses {
+  background-color: #f6f6f6;
+  margin-bottom: 0;
+  padding-bottom: 80px;
+}
+
 #coach {
-    background-color: #F8F8F8;
+  background-color: $background-color;
 }
 </style>
